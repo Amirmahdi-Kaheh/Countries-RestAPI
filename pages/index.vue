536491afad24e26deda9efbaaf7b56494e182ce2
mahdiar <kaheh.1384@gmail.com>
2023-06-27 16:46:18 +0330

    feat: using fuseJs for search component

--- pages/index.vue
+++ pages/index.vue
@@ -10,12 +10,12 @@
 
       <div>
         <countries-filter />
-        <countries-sort />
+        <countries-sort :name="sort.name" :population="sort.population" @sort-name="sortedByName" @sort-population="sortedByPopulation" />
       </div>
     </div>
 
     <div class="grid grid-cols-12 gap-6">
-      <country-item v-for="(item, index) in filteredCountry" :key="index" :data="item" />
+      <country-item v-for="(item, index) in filteredCountries" :key="index" :data="item" />
     </div>
   </div>
 </template>
@@ -26,6 +26,8 @@ import CountriesSearch from "~/components/pages/home/search-box";
 import CountriesFilter from "~/components/pages/home/filter";
 import CountriesSort from "~/components/pages/home/sort";
 import DicesIcon from "~/components/global/icons/dices-icon";
+import Fuse from 'fuse.js';
+
 export default {
   name: 'IndexPage',
   components: {DicesIcon, CountriesSort, CountriesFilter, CountriesSearch, CountryItem},
@@ -52,22 +54,86 @@ export default {
   data() {
     return {
       filter: {
-        search: ''
+        search: null
+      },
+      sort: {
+        name: 'none',
+        population: 'none',
       }
     }
   },
   computed:{
-    filteredCountry () {
-      try {
-        return this.countries.filter((country) => {
-          return country.name.common.toLowerCase().match(this.filter.search.toLowerCase());
+    filteredCountries () {
+      if(this.filter.search) {
+        const fuse = new Fuse(this.countries, {
+          keys: ['name.common'], // Specify the key(s) to search for
+          includeMatches: true,
+          shouldSort: true,
+          threshold: 0.5,
+          location: 0,
+          tokenize: true,
+          matchAllTokens: true,
+          findAllMatches: true,
+          minMatchCharLength: 1,
+          id: 'name'
         });
-      } catch {
-        return this.countries;
+
+        return fuse.search(this.filter.search).map(result => result.item);
+      }else {
+        return this.countries
       }
     }
   },
   methods: {
+    sortedByName() {
+      if (this.sort.name === "asc" || this.sort.name === "none") {
+        this.sort.name = "desc"
+        this.sort.population = "none";
+
+        function compare(a, b) {
+          if (a.name.common < b.name.common) {
+            return -1;
+          }
+          if (a.name.common > b.name.common) {
+            return 1;
+          }
+          return 0;
+        }
+
+        return this.countries.sort(compare)
+      } else if (this.sort.name === "desc") {
+        this.sort.name = "asc"
+
+        function compare(a, b) {
+          if (a.name.common > b.name.common) {
+            return -1;
+          }
+          if (a.name.common < b.name.common) {
+            return 1;
+          }
+          return 0;
+        }
+
+        return this.countries.sort(compare)
+      }
+    },
+    sortedByPopulation() {
+      if (this.sort.population === "asc" || this.sort.population === "none") {
+        this.sort.population = "desc";
+        this.sort.name = "none";
+
+        return this.countries.sort(function (a, b) {
+          return b.population - a.population;
+        })
+      } else if (this.sort.population === "desc") {
+        this.sort.population = "asc";
+        // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+        return this.countries.sort(function (a, b) {
+          return a.population - b.population;
+        })
+      }
+
+    },
     async getAll(){
       this.$axios.get('/v3.1/alpha?codes=IRN').then((response)=>{
         this.response = response.data
