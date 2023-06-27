9fc6aa62f8ca87425259b1b70bfb4929d3855f57
mahdiar <kaheh.1384@gmail.com>
2023-06-27 16:49:14 +0330

    feat: sort added to country list

---
+++ components/global/icons/sort-icon.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg width="22" height="22" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M3 16L7 20L11 16" class="stroke-gray" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M7 20V4" class="stroke-gray" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M11 4H21" class="stroke-gray" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M11 8H18" class="stroke-gray" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M11 12H15" class="stroke-gray" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "sort-icon"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/pages/home/sort.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex items-center gap-2">
+    <button @click="$emit('sort-population')" :class="['flex items-center gap-2 px-5 py-2.5 rounded-full active:scale-90 transition-all', {'bg-silver': population !== 'none'}]">
+      <sort-icon :class="[{'rotate-180':population==='asc'}]" />
+
+      <span>Population</span>
+    </button>
+
+    <button @click="$emit('sort-name')" :class="['flex items-center gap-2 px-5 py-2.5 rounded-full active:scale-90 transition-all', {'bg-silver': name !== 'none'}]">
+      <sort-icon :class="[{'rotate-180':name==='asc'}]" />
+
+      <span>Name</span>
+    </button>
+  </div>
+</template>
+
+<script>
+import SortIcon from "~/components/global/icons/sort-icon";
+export default {
+  name: "CountriesSort",
+  components: {SortIcon},
+  props: {
+    name: {
+      default: null,
+      type: String
+    },
+    population: {
+      default: null,
+      type: String
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
