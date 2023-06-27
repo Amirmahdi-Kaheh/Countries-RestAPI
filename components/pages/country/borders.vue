0ef7bebd5b49337425b3cfe9bf2973a8d4f83e18
mahdiar <kaheh.1384@gmail.com>
2023-06-27 18:57:57 +0330

    fix: validate data for small countries that don't have some of informations

--- components/pages/country/borders.vue
+++ components/pages/country/borders.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="my-10">
-    <h4 class="text-lg font-semibold mb-2">Borders</h4>
+    <h4 v-if="data.length > 0" class="text-lg font-semibold mb-2">Borders Countries</h4>
     <div class="flex flex-wrap gap-2">
       <nuxt-link v-for="(border, index) in data" :key="index" :to="border" class="px-6 py-2.5 rounded-xl text-gray border-2 border-silver">{{border}}</nuxt-link>
     </div>
