240832e5301847f1166b3cb89eaac3df5329d896
mahdiar <kaheh.1384@gmail.com>
2023-06-27 16:48:47 +0330

    feat: header link to homepage

--- components/layouts/TheHeader.vue
+++ components/layouts/TheHeader.vue
@@ -1,12 +1,12 @@
 <template>
   <header class="sticky top-0 w-full py-8 bg-white z-30 shadow-2xl shadow-silver">
     <div class="container">
-      <div class="flex items-center gap-4">
-        <img src="~/static/icon.png" alt="">
+      <nuxt-link to="/" class="flex items-center gap-4 group">
+        <img src="~/static/icon.png" class="group-hover:animate-spin" alt="">
 
         <!--  #WEBSITE TITLE    -->
         <h1 class="text-xl font-semibold">Where in the World ?</h1>
-      </div>
+      </nuxt-link>
     </div>
   </header>
 </template>
