a40ec0d26f4386099f76b96f41859f74ebf1a02f
mahdiar <kaheh.1384@gmail.com>
2023-06-27 03:48:35 +0330

    feat: get all countries data from asyncData

---
+++ components/pages/home/country-item.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="col-span-3 flex flex-col justify-between rounded-xl overflow-hidden p-4 bg-white transition-all">
+    <div>
+      <img class="w-full aspect-[3/2] object-cover rounded-xl border border-silver" :src="data.flags.png" alt="" loading="lazy">
+
+      <div class="mt-4">
+        <h4 class="text-lg font-semibold">{{data.name.common}}</h4>
+        <p class="text-sm text-gray w-full truncate" :title="data.name.official">{{data.name.official}}</p>
+      </div>
+    </div>
+
+    <div class="flex flex-col gap-1 mt-4 text-sm">
+      <div>
+        <b>Population: </b>
+        <span>{{data.population.toLocaleString()}}</span>
+      </div>
+
+      <div>
+        <b>Region: </b>
+        <span>{{data.region}}</span>
+      </div>
+
+      <div>
+        <b>Capital: </b>
+        <span v-for="capital in data.capital">{{capital}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "country-item",
+  props: {
+    data: {
+      default: null,
+      type: Object
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,37 @@
 <template>
   <div>
-    Countries RestAPI
+    <div class="grid grid-cols-12 gap-6">
+      <country-item v-for="(item, index) in countries" :key="index" :data="item" />
+    </div>
   </div>
 </template>
 
 <script>
+import CountryItem from "~/components/pages/home/country-item";
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: {CountryItem},
+  async asyncData ({ app, store, error }) {
+    try {
+      const [
+        countriesAllData
+      ] = await Promise.all([
+        app.$axios.get('/v3.1/all')
+      ]);
+
+      return {
+        countries: countriesAllData.data
+      };
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  methods: {
+    async getAll(){
+      this.$axios.get('/v3.1/name/Grmany').then((response)=>{
+        this.response = response.data
+      })
+    }
+  }
 }
 </script>
