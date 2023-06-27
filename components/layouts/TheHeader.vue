99a5f0b50725dd89f300a8b3ebccdf837ce39697
mahdiar <kaheh.1384@gmail.com>
2023-06-27 11:48:38 +0330

    feat: minor change in header visual

--- components/layouts/TheHeader.vue
+++ components/layouts/TheHeader.vue
@@ -1,15 +1,11 @@
 <template>
-  <header class="sticky top-0 w-full py-8 bg-white">
+  <header class="sticky top-0 w-full py-8 bg-white z-30 shadow-2xl shadow-silver">
     <div class="container">
       <div class="flex items-center gap-4">
         <img src="~/static/icon.png" alt="">
 
         <!--  #WEBSITE TITLE    -->
         <h1 class="text-xl font-semibold">Where in the World ?</h1>
-
-        <button class="px-4 py-2 rounded-full text-sm text-gray bg-silver">
-          Take me to a random Place!
-        </button>
       </div>
     </div>
   </header>
