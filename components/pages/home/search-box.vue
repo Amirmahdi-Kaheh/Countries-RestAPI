7fa6100add67f2487aeaf8f8249f0af43b34adb5
mahdiar <kaheh.1384@gmail.com>
2023-06-27 11:51:08 +0330

    feat: search country by name added && create function to pick a random country and show detail

---
+++ components/pages/home/search-box.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="relative flex items-center">
+    <input v-model="formData.search" type="text" placeholder="Search for a Country ..." class="form-control pl-14" @keypress.enter="test">
+
+    <search-icon class="absolute left-4 w-6" />
+  </div>
+</template>
+
+<script>
+import SearchIcon from "~/components/global/icons/search-icon";
+export default {
+  name: "CountriesSearch",
+  components: {SearchIcon},
+  data () {
+    return {
+      formData: {
+        search: ''
+      }
+    }
+  },
+  watch: {
+    'formData.search' (newVal) {
+      this.$emit('submit', newVal)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,40 @@
 <template>
   <div>
+    <div class="flex mb-6 items-center justify-between pb-6 border-b-2 border-b-silver">
+      <div class="flex items-center gap-4">
+        <countries-search @submit="(value)=> filter.search = value" />
+        <button class="p-3.5 rounded-xl bg-white active:scale-90 transition-all" @click="randomChoice">
+          <dices-icon />
+        </button>
+      </div>
+
+      <div>
+        <countries-filter />
+        <countries-sort />
+      </div>
+    </div>
+
     <div class="grid grid-cols-12 gap-6">
-      <country-item v-for="(item, index) in countries" :key="index" :data="item" />
+      <country-item v-for="(item, index) in filteredCountry" :key="index" :data="item" />
     </div>
   </div>
 </template>
 
 <script>
 import CountryItem from "~/components/pages/home/country-item";
+import CountriesSearch from "~/components/pages/home/search-box";
+import CountriesFilter from "~/components/pages/home/filter";
+import CountriesSort from "~/components/pages/home/sort";
+import DicesIcon from "~/components/global/icons/dices-icon";
 export default {
   name: 'IndexPage',
-  components: {CountryItem},
+  components: {DicesIcon, CountriesSort, CountriesFilter, CountriesSearch, CountryItem},
   async asyncData ({ app, store, error }) {
     try {
       const [
         countriesAllData
       ] = await Promise.all([
-        app.$axios.get('/v3.1/all?fields=name,flags,population,region,capital')
+        app.$axios.get('/v3.1/all?fields=name,flags,population,region,capital,cca3')
       ]);
 
       return {
@@ -26,14 +44,38 @@ export default {
       throw err
     }
   },
-  created() {
-    // this.getAll()
+  head () {
+    return {
+      title: 'Where in the World ?'
+    }
+  },
+  data() {
+    return {
+      filter: {
+        search: ''
+      }
+    }
+  },
+  computed:{
+    filteredCountry () {
+      try {
+        return this.countries.filter((country) => {
+          return country.name.common.toLowerCase().match(this.filter.search.toLowerCase());
+        });
+      } catch {
+        return this.countries;
+      }
+    }
   },
   methods: {
     async getAll(){
-      this.$axios.get('/v3.1/alpha?codes=400').then((response)=>{
+      this.$axios.get('/v3.1/alpha?codes=IRN').then((response)=>{
         this.response = response.data
       })
+    },
+    randomChoice() {
+      const randomCountry = this.countries[Math.floor(Math.random()*this.countries.length)]
+      this.$router.push(`/countries/${randomCountry.cca3}`)
     }
   }
 }
