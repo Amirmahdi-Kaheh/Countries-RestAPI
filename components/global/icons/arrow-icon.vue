7b84d07b765859964bdfdf78153538880c025489
mahdiar <kaheh.1384@gmail.com>
2023-06-27 16:49:52 +0330

    fix: arrow fixed

--- components/global/icons/arrow-icon.vue
+++ components/global/icons/arrow-icon.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="35" height="35" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M15 18L9 12L15 6" class="stroke-gray" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
   </svg>
 </template>
