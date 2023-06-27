fdcfbb0fc08d570e2274ceb6dba6ef387aa4397f
mahdiar <kaheh.1384@gmail.com>
2023-06-27 03:46:20 +0330

    feat: add theme color palette to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,30 @@ module.exports = {
     "./nuxt.config.{js,ts}",
   ],
   theme: {
-    extend: {},
+    container: {
+      // you can configure the container to be centered
+      center: true,
+    },
+    colors: {
+      transparent: "transparent",
+      blue: {
+        dark: {
+          DEFAULT: 'hsl(209, 23%, 22%)', // default stand for light mode
+          night: '' // dark mode color
+        },
+        darken: {
+          DEFAULT: '',
+          night: 'hsl(207, 26%, 17%)'
+        }
+      },
+      white: '#FFFFFF',
+      gray: 'hsl(0, 0%, 52%)',
+      background: {
+        DEFAULT: '#FAFAFAFF',
+        dark: '#202C37FF'
+      },
+      silver: '#eee'
+    },
   },
   plugins: [],
 }
